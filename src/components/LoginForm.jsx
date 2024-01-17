b8374ad18cf01c2f1a639bfde75b3363627947a1
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-01-17 11:09:30 +0100

    css arrangements

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -29,7 +29,7 @@ const Modal = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">BLABLAPP</h1>
+        <img src="../images/logo.png" className="title" />
         <form onSubmit={handleSubmit}>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
